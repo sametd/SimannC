cf8ac9e6b81ff5eb4ea45641868c375c3a948105
Samet Demir <sametdemir@gmail.com>
2018-10-03 01:04:46 +0300

    En basit haliyle tanimlar

---
+++ config.h
@@ -0,0 +1,16 @@
+//DOC max: maksimizasyon mu minimizasyon mu secimi. (0 minimizasyon)
+#define max 0
+//DOC eps: epsilon kriteri
+#define eps 0.001
+//DOC rt: sicakligin dusme katsayisi (ornek: yeni_t = t * rt)
+#define rt 0.85
+//DOC ns: NS parametresi NS*N iterasyon boyunca ayni adim
+//DOC     uzunlugunda parametre aramasi yapiliyor.
+#define ns 40
+//DOC nt: NT parametresi ayni sicaklikta kac defa adim uzunlugu
+//DOC     ayarlanarak NS*N parametre aramasi yapilacagini ifade eder.
+//DOC     Yani NT defa ayarlanan adim uzunluklarinin herbirinde
+//DOC     NS*N defa parametre aramasi yapilir.
+#define nt 40
+//DOC t:  Baslangic sicakligidir.
+#define t 5.0
