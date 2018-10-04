4a0a5f8e9cbd53daccac31b2dddef7ad2eb2dd46
Samet Demir <sametdemir@gmail.com>
2018-10-05 01:22:24 +0300

    Ayar dosyasi artik daha kullanisli

--- config.h
+++ config.h
@@ -1,16 +1,22 @@
-//DOC max: maksimizasyon mu minimizasyon mu secimi. (0 minimizasyon)
+// DOC  Nparam: parametre sayisi
+#define Nparam 2
+// DOC neps: epsilon degeri ile baglantili olarak, kac defa hic degisim
+// DOC       olmuyorsa onu sayarak programin bitis durumunu ayarliyor.
+// DOC       4 degeri oneriliyor
+#define neps 4
+// DOC max: maksimizasyon mu minimizasyon mu secimi. (0 minimizasyon)
 #define max 0
-//DOC eps: epsilon kriteri
+// DOC eps: epsilon kriteri
 #define eps 0.001
-//DOC rt: sicakligin dusme katsayisi (ornek: yeni_t = t * rt)
+// DOC rt: sicakligin dusme katsayisi (ornek: yeni_t = t * rt)
 #define rt 0.85
-//DOC ns: NS parametresi NS*N iterasyon boyunca ayni adim
-//DOC     uzunlugunda parametre aramasi yapiliyor.
+// DOC ns: NS parametresi NS*N iterasyon boyunca ayni adim
+// DOC     uzunlugunda parametre aramasi yapiliyor.
 #define ns 40
-//DOC nt: NT parametresi ayni sicaklikta kac defa adim uzunlugu
-//DOC     ayarlanarak NS*N parametre aramasi yapilacagini ifade eder
-//DOC     Yani NT defa ayarlanan adim uzunluklarinin herbirinde
-//DOC     NS*N defa parametre aramasi yapilir
+// DOC nt: NT parametresi ayni sicaklikta kac defa adim uzunlugu
+// DOC     ayarlanarak NS*N parametre aramasi yapilacagini ifade eder
+// DOC     Yani NT defa ayarlanan adim uzunluklarinin herbirinde
+// DOC     NS*N defa parametre aramasi yapilir
 #define nt 40
-//DOC t:  Baslangic sicakligidir
+// DOC t:  Baslangic sicakligidir
 #define t 5.0
