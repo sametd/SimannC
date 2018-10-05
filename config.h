1c6f558a076f80c205ef35181ad6471590e3c449
Mulayim <sametdemir@gmail.com>
2018-10-05 17:58:57 +0300

    Sicaklik programda guncellendigi icin tanim degisti

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
 // DOC max: maksimizasyon mu minimizasyon mu secimi. (0 minimizasyon)
 #define max 0
 // DOC eps: epsilon kriteri
-#define eps 0.001
+#define eps 0.01
 // DOC rt: sicakligin dusme katsayisi (ornek: yeni_t = t * rt)
 #define rt 0.85
 // DOC ns: NS parametresi NS*N iterasyon boyunca ayni adim
@@ -19,4 +19,4 @@
 // DOC     NS*N defa parametre aramasi yapilir
 #define nt 40
 // DOC t:  Baslangic sicakligidir
-#define t 5.0
+#define startTemp 5.0
