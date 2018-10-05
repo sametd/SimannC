1070372d78cffd277604a5e409eee22645eb78ab
Mulayim <sametdemir@gmail.com>
2018-10-05 17:59:20 +0300

    stdio simdilik gereksizdi kaldirildi

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "config.h"
 #include "simann.h"
 
-int main(int argc, char *argv[]) 
-{
+int main(int argc, char *argv[]) {
   // DOC  p: parametre arrayi
   // DOC  f: fonksiyon degeri
+  srand(time(NULL));
   double param[Nparam];
   double fval = 0.;
-  simann(param,&fval);
+  simann(param, &fval);
 
   return 0;
 }
